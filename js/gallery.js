f2a797182b4a60a39d15089e992321bcb3877d82
JPJ <CopenhagenChainGang@gmail.com>
2022-05-10 17:31:34 -0700

    Add code to preload large images

--- js/gallery.js
+++ js/gallery.js
@@ -1,18 +1,18 @@
 // Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery.
+// and respond appropriately on click.
 function activateGallery() {
   let thumbnails  = document.querySelectorAll("#gallery-thumbs > div > img");
   let mainImage   = document.querySelector("#gallery-photo img");
-    //Image info to be updated.
-  let galleryInfo = document.querySelector("#gallery-info");
-  let title       = galleryInfo.querySelector(".title");
-  let description = galleryInfo.querySelector(".description");
 
-// The main task is to attach an event listener to each image in the gallery.
-// and respond appropriately on click.
+
   thumbnails.forEach(function(thumbnail) {
+    //Preload large images.
+    let newImageSrc  = thumbnail.dataset.largeVersion;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc;
     thumbnail.addEventListener("click", function() {
       // Set clicked image as main image.
-      let newImageSrc = thumbnail.dataset.largeVersion;
       mainImage.setAttribute("src", newImageSrc);
 
       //Change which image is current.
@@ -20,9 +20,13 @@ function activateGallery() {
       thumbnail.parentNode.classList.add("current");
 
       //Update image info.
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
       title.innerHTML       = thumbnail.dataset.title;
       description.innerHTML = thumbnail.dataset.description;
 
-});
+    });
   });
 }
